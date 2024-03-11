e8948b1d1e9433b002ef6eeb68f4dab8ec82fcef
Cloud User <ec2-user@ip-172-31-95-2.ec2.internal>
2024-03-11 21:26:05 +0000

    adicionar exemplo do professor

--- lab02/ex01_03.c
+++ lab02/ex01_03.c
@@ -3,15 +3,14 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#define BUFFER_SIZE 4
+#define BUFFER_SIZE 4  // buffer size for read(0 and write()
 
-
-int  value = 5;
+int value = 5;
 
 int main()
 {
 	pid_t pid;
-        int p[2];
+        int p[2]; // 2 index array for pipeline usage
 
 	if (pipe(p) < 0) { /* error in pipe creation */
 	        exit(1);
@@ -20,16 +19,19 @@ int main()
 	pid = fork();
 
 	if (pid == 0) { /* child process */
+		close(p[0]); // closing file descriptor for reading (child)
 		printf("Entrei no filho!\n");
 		value += 15;
-		write(p[1], &value, BUFFER_SIZE);
-		close(p[1]);
+		write(p[1], &value, BUFFER_SIZE); // writing in pipe file descriptor
+		close(p[1]); // closing fd for writing (child)
 		printf ("CHILD: value = %d\n",value); /* LINE A */
 		return 0;
 	}
 	else if (pid > 0) { /* parent process */
 		wait(NULL);
-		read(p[0], &value, BUFFER_SIZE);
+		read(p[0], &value, BUFFER_SIZE); // reading fd from pipe
+		close(p[1]); // closing fd for writing (parent)
+		close(p[0]); // closing fd for reading (parent)
 		printf ("PARENT: value = %d\n",value); /* LINE A */
 		return 0;
 	}
