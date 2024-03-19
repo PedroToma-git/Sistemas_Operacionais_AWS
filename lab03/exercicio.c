dea91cb7bf4e42219554bb41fd956e2d2d9d55ab
Cloud User <ec2-user@ip-172-31-83-101.ec2.internal>
2024-03-19 00:23:07 +0000

    editar exercicio

--- lab03/exercicio.c
+++ lab03/exercicio.c
@@ -12,11 +12,11 @@
 #define FIBER_STACK 1024*64
 
 // The child thread will execute this function
-void *threadFunction ( void* var )
+void *threadFunction ( void *var )
 {
-     int variavel;
-     variavel = (int)var;
-     printf("%d", variavel);
+     int *variavel;
+     variavel = (int *)var;
+     printf("%d", *variavel);
      printf("child thread exiting\n");
 }
 
@@ -28,7 +28,7 @@ int main()
      printf( "Creating child thread\n" );
 
      // Call the clone system call to create the child thread
-     pthread_create(&thread, NULL, threadFunction, (void *)var);
+     pthread_create(&thread, NULL, threadFunction, (void *)&var);
 
      // Wait for the child thread to exit
 
